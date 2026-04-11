e7fedf2306633da850f2d88ea132f7d0503f3c04
Yashwanth-NS-07 <yashns1859@gmail.com>
2026-04-12 01:08:06 +0530

    codecrafters submit [skip ci]

--- src/main/java/ProcessRequest.java
+++ src/main/java/ProcessRequest.java
@@ -6,23 +6,36 @@ import java.util.Optional;
 public class ProcessRequest {
     private static final ByteBuffer byteBuffer = ByteBuffer.allocate(1000);
     public static void process(Request request, SocketChannel channel) throws IOException {
-        String value = request.getParameter(0);
-        if(value.equalsIgnoreCase("ping")) {
+        String cmd = request.getParameter(0);
+        if(cmd.equalsIgnoreCase("ping")) {
             byteBuffer.clear();
             byteBuffer.put("+PONG\r\n".getBytes());
             byteBuffer.flip();
             channel.write(byteBuffer);
-        } else if (value.equals("ECHO")) {
+        } else if (cmd.equals("ECHO")) {
             Response response = new Response(1);
             response.add(request.getParameter(1));
             Response.writeResponse(response, channel, byteBuffer);
-        } else if (value.equals("SET")) {
-            Store.put(request.getParameter(1), request.getParameter(2));
+        } else if (cmd.equals("SET")) {
             byteBuffer.clear();
             byteBuffer.put("+OK\r\n".getBytes());
             byteBuffer.flip();
+            String key = request.getParameter(1);
+            String value = request.getParameter(2);
+            if(request.getParameterCount() == 3) {
+                Store.put(key, value, -1);
+            } else {
+                String arg = request.getParameter(4);
+                long expiryTime = Integer.parseInt(request.getParameter(5));
+                if(arg.equals("EX")) {
+                    long liveTime = expiryTime * 1000L;
+                    Store.put(key, value, liveTime);
+                } else if(arg.equalsIgnoreCase("PX")) {
+                    Store.put(key, value, expiryTime);
+                }
+            }
             channel.write(byteBuffer);
-        } else if(value.equals("GET")) {
+        } else if(cmd.equals("GET")) {
             Optional<String> optionalVal = Store.get(request.getParameter(1));
             if(optionalVal.isEmpty()) {
                 byteBuffer.clear();
--- src/main/java/Store.java
+++ src/main/java/Store.java
@@ -3,16 +3,39 @@ import java.util.Map;
 import java.util.Optional;
 
 public class Store {
-    private static final Map<String, String> map;
+    private static final Map<String, Value> map;
     static {
         map = new HashMap<>();
     }
-    public static void put(String key, String value) {
-        map.put(key, value);
+    public static void put(String key, String value, long liveTime) {
+        map.put(key, new Value(value, liveTime));
     }
     public static Optional<String> get(String key) {
-        if(map.containsKey(key))
-            return Optional.of(map.get(key));
+        if(map.containsKey(key)) {
+            Value value = map.get(key);
+            if(value.timestamp == -1) {
+                return Optional.of(value.value);
+            } else {
+                long duration = System.currentTimeMillis() - value.timestamp;
+                if(duration < value.liveTime) {
+                    return Optional.of(value.value);
+                } else {
+                    map.remove(key);
+                }
+            }
+        }
         return Optional.empty();
+    }
+
+    private static class Value {
+        private long timestamp;
+        private long liveTime;
+        private String value;
+        // liveTime value expected in millis
+        public Value(String value,long liveTime) {
+            this.value = value;
+            this.liveTime = liveTime;
+            this.timestamp = System.currentTimeMillis();
+        }
     }
 }
