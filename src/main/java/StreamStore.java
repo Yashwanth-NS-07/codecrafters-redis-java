23216f01b13055e4af355b14dc171b5e42286e5f
Yashwanth-NS-07 <yashns1859@gmail.com>
2026-04-15 22:21:48 +0530

    codecrafters submit [skip ci]

--- src/main/java/StreamStore.java
+++ src/main/java/StreamStore.java
@@ -16,9 +16,17 @@ public class StreamStore {
         stream.get(streamName).add(values);
     }
 
+    private static Map<String, String> getLast(String streamName) {
+        List<Map<String, String>> streamList = stream.get(streamName);
+        return streamList.get(streamList.size() - 1);
+    }
+
     public static void handleXADD(Request request, ByteBuffer byteBuffer) {
         String streamName = request.getParameter(1);
         String id = request.getParameter(2);
+        if(!isIdProper(streamName, id, byteBuffer)) {
+            return;
+        }
         Map<String, String> map = new HashMap<>();
         map.put("Id", id);
         for(int i = 3; i < request.getParameterCount(); i+=2) {
@@ -30,5 +38,27 @@ public class StreamStore {
         Response response = new Response();
         response.add(id);
         ResponseUtils.writeBulkStringResponse(response, byteBuffer);
+    }
+
+    private static boolean isIdProper(String streamName, String id, ByteBuffer byteBuffer) {
+        String[] parts = id.split("-");
+        long milli = Long.parseLong(parts[0]);
+        int seq = Integer.parseInt(parts[1]);
+        if(isStreamExists(streamName)) {
+            String lastId = getLast(streamName).get("Id");
+            String[] lastRecordParts = lastId.split("-");
+            long lastRecordMilli = Long.parseLong(lastRecordParts[0]);
+            int lastRecordSeq = Integer.parseInt(lastRecordParts[1]);
+            if(milli <= lastRecordMilli && seq <= lastRecordSeq) {
+                byteBuffer.put("-ERR The ID specified in XADD is equal or smaller than the target stream top item\r\n".getBytes());
+                return false;
+            }
+        } else {
+            if(milli <= 0 || seq <= 0) {
+                byteBuffer.put("-ERR The ID specified in XADD must be greater than 0-0\r\n".getBytes());
+                return false;
+            }
+        }
+        return true;
     }
 }
