addb09f8ebe7176effa107e15d78e4a9bfc81028
Yashwanth-NS-07 <yashns1859@gmail.com>
2026-04-25 18:38:27 +0530

    codecrafters submit [skip ci]

--- src/main/java/TransactionManager.java
+++ src/main/java/TransactionManager.java
@@ -2,29 +2,20 @@ import java.io.IOException;
 import java.net.SocketAddress;
 import java.nio.ByteBuffer;
 import java.nio.channels.SocketChannel;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class TransactionManager {
     private static final Map<SocketAddress, List<Request>> transactions = new HashMap<>();
     public static void handleRequest(Request request, SocketChannel channel) throws IOException {
         if("MULTI".equalsIgnoreCase(request.getParameter(0))) {
             transactions.put(channel.getRemoteAddress(), new ArrayList<>());
-            ByteBuffer tempByteBuffer = ByteBuffer.allocate(10);
-            tempByteBuffer.put("+OK\r\n".getBytes());
-            tempByteBuffer.flip();
-            channel.write(tempByteBuffer);
+            writeToChannel("+OK\r\n", channel);
             return;
         } else if(
                 "EXEC".equalsIgnoreCase(request.getParameter(0)) &&
                 !transactions.containsKey(channel.getRemoteAddress())
         ) {
-                ByteBuffer tempByteBuffer = ByteBuffer.allocate(50);
-                tempByteBuffer.put("-ERR EXEC without MULTI\r\n".getBytes());
-                tempByteBuffer.flip();
-                channel.write(tempByteBuffer);
+                writeToChannel("-ERR EXEC without MULTI\r\n", channel);
                 return;
         }
 
@@ -34,7 +25,9 @@ public class TransactionManager {
             if("EXEC".equalsIgnoreCase(request.getParameter(0))) {
                 processAllQueuedRequest(channel);
             } else {
+                // queuing
                 transactions.get(clientAddress).add(request);
+                writeToChannel("+QUEUED\r\n", channel);
             }
         } else {
             // normal request
@@ -46,14 +39,20 @@ public class TransactionManager {
         List<Request> requests = transactions.get(channel.getRemoteAddress());
         transactions.remove(channel.getRemoteAddress());
         if(requests.isEmpty()) {
-            ByteBuffer tempByteBuffer = ByteBuffer.allocate(5);
-            tempByteBuffer.put("*0\r\n".getBytes());
-            tempByteBuffer.flip();
-            channel.write(tempByteBuffer);
+            writeToChannel("*0\r\n", channel);
             return;
         }
         for(Request request: requests) {
             ProcessRequest.process(request, channel);
+        }
+    }
+
+    private static void writeToChannel(String value, SocketChannel channel) throws IOException {
+        Objects.requireNonNull(value);
+        ByteBuffer byteBuffer = ByteBuffer.wrap(value.getBytes());
+        byteBuffer.flip();
+        while(byteBuffer.hasRemaining()) {
+            channel.write(byteBuffer);
         }
     }
 }
