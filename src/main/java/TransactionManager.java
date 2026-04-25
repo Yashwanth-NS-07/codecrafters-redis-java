4f2f21fad54215eff09bfde888397e56220e435d
Yashwanth-NS-07 <yashns1859@gmail.com>
2026-04-25 22:55:03 +0530

    codecrafters submit [skip ci]

--- src/main/java/TransactionManager.java
+++ src/main/java/TransactionManager.java
@@ -5,40 +5,51 @@ import java.nio.channels.SocketChannel;
 import java.util.*;
 
 public class TransactionManager {
-    private static final Map<SocketAddress, List<Request>> transactions = new HashMap<>();
-    public static void handleRequest(Request request, SocketChannel channel) throws IOException {
-        if("MULTI".equalsIgnoreCase(request.getParameter(0))) {
-            transactions.put(channel.getRemoteAddress(), new ArrayList<>());
-            writeToChannel("+OK\r\n", channel);
-            return;
-        } else if(
-                "EXEC".equalsIgnoreCase(request.getParameter(0)) &&
-                !transactions.containsKey(channel.getRemoteAddress())
-        ) {
-                writeToChannel("-ERR EXEC without MULTI\r\n", channel);
-                return;
-        }
 
-        // checking if the client is having ongoing transaction
+    private static final Map<SocketAddress, List<Request>> transactions = new HashMap<>();
+
+    public static void handleRequest(Request request, SocketChannel channel) throws IOException {
+
         SocketAddress clientAddress = channel.getRemoteAddress();
-        if(transactions.containsKey(clientAddress)) {
-            if("EXEC".equalsIgnoreCase(request.getParameter(0))) {
-                processAllQueuedRequest(channel);
-            } else {
-                // queuing
-                transactions.get(clientAddress).add(request);
-                writeToChannel("+QUEUED\r\n", channel);
+        String val = request.getParameter(0);
+        switch (val) {
+            case "MULTI": {
+                transactions.put(clientAddress, new ArrayList<>());
+                writeToChannel("+OK\r\n", channel);
+                return;
             }
-        } else {
-            // normal request
-            String response = ProcessRequest.process(request, channel);
-            writeToChannel(response, channel);
+            case "EXEC": {
+                if(!transactions.containsKey(clientAddress)) {
+                    writeToChannel("-ERR EXEC without MULTI\r\n", channel);
+                    return;
+                }
+                List<Request> queuedRequests = transactions.remove(clientAddress);
+                processAllQueuedRequest(queuedRequests, channel);
+                return;
+            }
+            case "DISCARD": {
+                if(!transactions.containsKey(clientAddress)) {
+                    writeToChannel("-ERR DISCARD without MULTI\r\n", channel);
+                    return;
+                }
+                transactions.remove(clientAddress);
+                writeToChannel("+OK\r\n", channel);
+            }
+            default: {
+                if(transactions.containsKey(clientAddress)) {
+                    // queuing
+                    transactions.get(clientAddress).add(request);
+                    writeToChannel("+QUEUED\r\n", channel);
+                } else {
+                    // normal request
+                    String response = ProcessRequest.process(request, channel);
+                    writeToChannel(response, channel);
+                }
+            }
         }
     }
 
-    private static void processAllQueuedRequest(SocketChannel channel) throws IOException {
-        List<Request> requests = transactions.get(channel.getRemoteAddress());
-        transactions.remove(channel.getRemoteAddress());
+    private static void processAllQueuedRequest(List<Request> requests, SocketChannel channel) throws IOException {
         if(requests.isEmpty()) {
             writeToChannel("*0\r\n", channel);
             return;
