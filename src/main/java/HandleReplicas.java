7298dadce3623608dbdc5bf9579738c4b2d5797e
Yashwanth-NS-07 <yashns1859@gmail.com>
2026-04-26 20:36:22 +0530

    codecrafters submit [skip ci]

--- src/main/java/HandleReplicas.java
+++ src/main/java/HandleReplicas.java
@@ -1,11 +1,17 @@
 public class HandleReplicas {
+    private static final String hardCodedRDBBinary = "UkVESVMwMDEx+glyZWRpcy12ZXIFNy4yLjD6CnJlZGlzLWJpdHPAQPoFY3RpbWXCbQi8ZfoIdXNlZC1tZW3CsMQQAPoIYW9mLWJhc2XAAP/wbjv+wP9aog==";
     public static String handleREPLCONF(Request request) {
         return "+OK\r\n";
     }
     public static String handlePSYNC(Request request) {
-        String sb = "+FULLRESYNC " +
-                InfoHandler.get("master_replid") +
-                " 0\r\n";
-        return sb;
+        StringBuilder sb = new StringBuilder();
+        sb.append("+FULLRESYNC ");
+        sb.append(InfoHandler.get("master_replid"));
+        sb.append(" 0\r\n");
+        sb.append("$");
+        sb.append(hardCodedRDBBinary.length());
+        sb.append("\r\n");
+        sb.append(hardCodedRDBBinary);
+        return sb.toString();
     }
 }
