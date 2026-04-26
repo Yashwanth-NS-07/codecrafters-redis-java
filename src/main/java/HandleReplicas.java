fd5ee3697ded820f96477c50e91a8ec890f39944
Yashwanth-NS-07 <yashns1859@gmail.com>
2026-04-26 20:42:45 +0530

    codecrafters submit [skip ci]

--- src/main/java/HandleReplicas.java
+++ src/main/java/HandleReplicas.java
@@ -1,5 +1,11 @@
+import java.util.Base64;
+
 public class HandleReplicas {
-    private static final String hardCodedRDBBinary = "UkVESVMwMDEx+glyZWRpcy12ZXIFNy4yLjD6CnJlZGlzLWJpdHPAQPoFY3RpbWXCbQi8ZfoIdXNlZC1tZW3CsMQQAPoIYW9mLWJhc2XAAP/wbjv+wP9aog==";
+    private static final String rdbString;
+    static {
+        String s = "UkVESVMwMDEx+glyZWRpcy12ZXIFNy4yLjD6CnJlZGlzLWJpdHPAQPoFY3RpbWXCbQi8ZfoIdXNlZC1tZW3CsMQQAPoIYW9mLWJhc2XAAP/wbjv+wP9aog==";
+        rdbString = new String(Base64.getDecoder().decode(s));
+    }
     public static String handleREPLCONF(Request request) {
         return "+OK\r\n";
     }
@@ -9,9 +15,10 @@ public class HandleReplicas {
         sb.append(InfoHandler.get("master_replid"));
         sb.append(" 0\r\n");
         sb.append("$");
-        sb.append(hardCodedRDBBinary.length());
+        sb.append(rdbString.length());
         sb.append("\r\n");
-        sb.append(hardCodedRDBBinary);
+        sb.append(rdbString);
+;
         return sb.toString();
     }
 }
