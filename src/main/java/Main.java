53aac226920fc0cf5466d5d2265a34668caac5ea
Yashwanth-NS-07 <yashns1859@gmail.com>
2026-04-27 23:34:40 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -144,6 +144,7 @@ public class Main {
             throw new RuntimeException(e);
         }
         while(channel.read(byteBuffer) == 0);
+        System.out.println(byteBuffer.position());
 
 
         System.out.println("I am slave");
