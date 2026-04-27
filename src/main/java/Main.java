165eb41e65a05ff7626370308af7923a679b69d8
Yashwanth-NS-07 <yashns1859@gmail.com>
2026-04-27 23:02:18 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -136,6 +136,11 @@ public class Main {
             channel.write(byteBuffer);
         }
         byteBuffer.clear();
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
         while(channel.read(byteBuffer) == 0);
         return channel;
     }
